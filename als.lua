fb8df1b12ccec5c43ea6c8a2e98e968dac766468
Ayanokoji <104145422+Ayanok0ji@users.noreply.github.com>
2025-04-27 00:52:08 +0800

    Update als.lua

--- als.lua
+++ als.lua
@@ -71,7 +71,7 @@ credit.TextXAlignment = Enum.TextXAlignment.Right
 
 
 local running = false
-local selectedTime = 3600
+local selectedTime = 3550
 
 local function updateSliderPosition(timeLeft)
     local percentLeft = timeLeft / selectedTime
